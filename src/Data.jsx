b3fef6f5d781fef0c9dc55f7209f086d0f00c43b
leonielrnt <leonielaurent.31@gmail.com>
2024-04-29 18:52:30 +0200

    essais

--- src/Data.jsx
+++ src/Data.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 
 const Data = () => {
   const [Data, setData] = useState([]);
-  const url =
-    "https://api.themoviedb.org/3/movie/568?api_key=b4302baf8209ba84d052739967ae763f";
+  let movie_id = Math.random() * 1000;
+  movie_id = Math.round(movie_id);
+  const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=b4302baf8209ba84d052739967ae763f`;
   const getData = async (url) => {
     const rawData = await fetch(url);
     const jsonData = await rawData.json();
@@ -13,6 +14,20 @@ const Data = () => {
     getData(url);
   }, []);
   console.log(Data);
+
+  return (
+    <>
+      <div>
+        Le nom du film est {Data.title}
+        <br />
+        <img
+          src={`https://image.tmdb.org/t/p/w500${Data.poster_path}`}
+          alt={Data.title}
+          className="poster"
+        />
+      </div>
+    </>
+  );
 };
 
 export default Data;
