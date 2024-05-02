09dfde01fb67a438736c8a1387391904155bfbe2
Elsa Menegaux <elsa.menegaux@free.fr>
2024-05-02 19:09:57 +0200

    ménage

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,28 +1,3 @@
-// import React from "react";
-// import "./card.css";
-// import { Link } from "react-router-dom";
-
-// const Card = ({ movie }) => {
-//   return (
-//     <Link to={`/movies/${movie.id}`} className="link">
-//       <div className="content">
-//         <div className="movie_description">
-//           <h1>{movie.title}</h1> <br />
-//           <span>{movie.id}</span> <br />
-//           <span>{movie.overview}</span>
-//         </div>
-//         <img
-//           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-//           alt={movie.title}
-//           className="poster"
-//         />
-//       </div>
-//     </Link>
-//   );
-// };
-
-// export default Card;
-
 import React from "react";
 import "./card.css";
 import { Link } from "react-router-dom";
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,37 +1,3 @@
-// import React from "react";
-// import Card from "./Card";
-// import Header from "./Header";
-// import "./home.css";
-// import Data from "./Data";
-
-// const Movies = () => {
-//   const movieData = Data();
-
-//   return (
-//     <>
-//       <Header />
-//       <h2 className="title">Popular right now</h2>
-//       <div className="cards">
-//         <div className="row-container">
-//           {movieData.slice(0, 6).map((movie, i) => (
-//             <Card key={i} movie={movie} />
-//           ))}
-//         </div>
-//       </div>
-//       <h2 className="title">Users love these movies</h2>
-//       <div className="cards">
-//         <div className="row-container">
-//           {movieData.slice(6, 30).map((movie, i) => (
-//             <Card key={i} movie={movie} />
-//           ))}
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Movies;
-
 import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import Header from "./Header";
