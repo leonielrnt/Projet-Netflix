5d082da2c14a5b1fe1037f7934485b5411adfd59
leonielrnt <leonielaurent.31@gmail.com>
2024-05-02 10:30:18 +0200

    connexion entre les deux pages

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Card from "./Card";
+import Header from "./Header";
 import "./home.css";
 
 const Movies = () => {
   return (
     <>
-      <h1>Accueil</h1>
-      <div>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+    <h1>VIBE</h1>
+      <h2 className="title">Tous les films</h2>
+      <div className="content">
+        {Array.from({ length: 7 }).map((_, i) => (
+          <Card key={i} />
+        ))}
       </div>
+      <h2 className="title">Les films populaires</h2>
     </>
   );
 };
