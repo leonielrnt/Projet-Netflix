4c6adfd33dfd9b634ba737c7939f1a82a589f35d
Elsa Menegaux <elsa.menegaux@free.fr>
2024-05-02 21:14:40 +0200

    fixed problems on the homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,32 +5,43 @@ import "./home.css";
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+
+  const fetchData = async () => {
+    const API_key = "b4302baf8209ba84d052739967ae763f";
+
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${API_key}`
+    );
+
+    const data = await response.json();
+
+    const uniqueMovies = [...new Set(data.results)];
+
+    const shuffledMovies = uniqueMovies.sort(() => 0.5 - Math.random());
+
+    setMovies(shuffledMovies);
+  };
+
+  const fetchTopRatedMovies = async () => {
+    const API_key = "b4302baf8209ba84d052739967ae763f";
+
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_key}`
+    );
+
+    const data = await response.json();
+
+    const uniqueMovies = [...new Set(data.results)];
+
+    const shuffledMovies = uniqueMovies.sort(() => 0.5 - Math.random());
+
+    setTopRatedMovies(shuffledMovies);
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const API_key = "b4302baf8209ba84d052739967ae763f";
-      const response = await fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${API_key}`
-      );
-      const data = await response.json();
-      const all_movies = data.results;
-
-      // Create a set to store unique movie IDs
-      const uniqueMovieIds = new Set();
-
-      // Filter out duplicate movies and add them to the state
-      const uniqueMovies = all_movies.filter((movie) => {
-        if (!uniqueMovieIds.has(movie.id)) {
-          uniqueMovieIds.add(movie.id);
-          return true;
-        }
-        return false;
-      });
-
-      setMovies(uniqueMovies);
-    };
-
     fetchData();
+    fetchTopRatedMovies();
   }, []);
 
   return (
@@ -39,16 +50,16 @@ const Movies = () => {
       <h2 className="title">Popular right now</h2>
       <div className="cards">
         <div className="row-container">
-          {movies.slice(0, 6).map((movie, i) => (
-            <Card key={i} movie={movie} />
+          {movies.slice(0, 6).map((movie) => (
+            <Card key={movie.id} movie={movie} />
           ))}
         </div>
       </div>
       <h2 className="title">Users love these movies</h2>
       <div className="cards">
         <div className="row-container">
-          {movies.slice(6, 30).map((movie, i) => (
-            <Card key={i} movie={movie} />
+          {topRatedMovies.slice(0, 24).map((movie) => (
+            <Card key={movie.id} movie={movie} />
           ))}
         </div>
       </div>
