26605a0d0f19532ec0334c49a48980070e4f2a50
Elsa Menegaux <elsa.menegaux@free.fr>
2024-05-03 03:25:11 +0200

    fixed selection of movies on homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,29 +4,46 @@ import Header from "./Header";
 import "./home.css";
 
 const Movies = () => {
-  const [movies, setMovies] = useState([]);
+  const [popularMovies, setMovies] = useState([]);
   const [topRatedMovies, setTopRatedMovies] = useState([]);
 
   const fetchData = async () => {
     const API_key = "b4302baf8209ba84d052739967ae763f";
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${API_key}`
-    );
-    const data = await response.json();
-    const uniqueMovies = [...new Set(data.results)];
-    const shuffledMovies = uniqueMovies.sort(() => 0.5 - Math.random());
-    setMovies(shuffledMovies);
+
+    const totalPages = 10;
+    const popularMovies = [];
+
+    for (let page = 1; page <= totalPages; page++) {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_key}&page=${page}`
+      );
+      const data = await response.json();
+      popularMovies.push(...data.results);
+    }
+
+    const shuffledMovies = popularMovies.sort(() => 0.5 - Math.random());
+    setMovies(shuffledMovies.slice(0, 6));
   };
 
   const fetchTopRatedMovies = async () => {
     const API_key = "b4302baf8209ba84d052739967ae763f";
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_key}`
+
+    const totalPages = 10;
+    // nombre de pages de films popu à chercher. Nombre arbitraire.
+    const topRatedMovies = [];
+
+    for (let page = 1; page <= totalPages; page++) {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_key}&page=${page}`
+      );
+      const data = await response.json();
+      topRatedMovies.push(...data.results);
+    }
+
+    const shuffledTopRatedMovies = topRatedMovies.sort(
+      () => 0.5 - Math.random()
     );
-    const data = await response.json();
-    const uniqueMovies = [...new Set(data.results)];
-    const shuffledMovies = uniqueMovies.sort(() => 0.5 - Math.random());
-    setTopRatedMovies(shuffledMovies);
+    setTopRatedMovies(shuffledTopRatedMovies.slice(0, 28));
   };
 
   useEffect(() => {
@@ -39,13 +56,13 @@ const Movies = () => {
       <Header />
       <h2 className="title">Popular right now</h2>
       <div className="cards">
-        {movies.slice(0, 6).map((movie) => (
+        {popularMovies.map((movie) => (
           <Card key={movie.id} movie={movie} />
         ))}
       </div>
       <h2 className="title">Users love these movies</h2>
       <div className="cards">
-        {topRatedMovies.slice(0, 24).map((movie) => (
+        {topRatedMovies.map((movie) => (
           <Card key={movie.id} movie={movie} />
         ))}
       </div>
